c31553fa520ae710a4e3c61fb92d8361eb8a724c
Cepaqpum <Cepaqpum@land.ru>
2017-08-16 00:23:29 +0500

    COFFEERUN Chapter 10

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,10 +1,15 @@
 (function(window) {
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
-
+  var FormHandler = App.FormHandler;
   var truck = new Truck("NCC-1701", new DataStore());
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(truck.createOrder.bind(truck));
 
   window.truck = truck;
 })(window);
