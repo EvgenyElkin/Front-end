0784b3028895a1286ad9200e0a4097de3fd7f04e
Cepaqpum <Cepaqpum@land.ru>
2017-08-28 08:18:03 +0500

    TRACKER Chapter 20

--- tracker/app/router.js
+++ tracker/app/router.js
@@ -2,11 +2,20 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: config.locationType
 });
 
 Router.map(function() {
+  this.route('sightings', function() {
+    this.route('new');
+  });
+  this.route('sighting', function() {
+    this.route('edit');
+  });
+  this.route('cryptids');
+  this.route('cryptid');
+  this.route('witnesses');
+  this.route('witness');
 });
 
 export default Router;
